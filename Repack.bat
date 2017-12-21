e34488c54f6a83138bb3162de2e62a95847961e3
Uupo03 <31313784+Uupo03@users.noreply.github.com>
2017-12-21 16:41:50 +0200

    Update stuff
    *add a [y/n] question to the warning
    *bug fixes

--- Repack.bat
+++ Repack.bat
@@ -1,15 +1,51 @@
 @echo OFF
-if not exist "current.nds" ECHO Error, no current.nds, close this window
-if not exist "current.nds" pause >nul
-if exist "repacked.nds" ECHO !WARNING! Pressing any key will OVERWRITE the repcked.nds in this directory. !WARNING!
-if exist "repacked.nds" pause >nul
+
+ECHO /\~~~~~~~~~~=--NDS ROM Repack Script by Uupo03--=~~~~~~~~~~/\
+
+if not exist "*.nds" ECHO !Error! No nds ROM to repack, press any key to close this window. 2>nul
+if not exist "*.nds" pause >nul 2>nul
+if not exist "*.nds" exit >nul 2>nul
+if not exist "repacked.nds" goto :script >nul 2>nul
+ 
+:choice
+set /P c=!Error! A "repacked.nds" already exists in this directory, do you want to delete it?[Y/N]?
+if /I "%c%" EQU "Y" goto :choice_yes
+if /I "%c%" EQU "N" goto :choice_no
+goto :choice
+
+:choice_yes
+
+echo Deleteing repacked.nds
+DEL repacked.nds
+echo DONE! Press any key to continue.
+pause >nul 2>nul
+goto :script >nul 2>nul
+
+:choice_no
+
+echo Press any key to close this window.
+pause >nul 2>nul
+exit >nul 2>nul
+
+)
+:script
+
+if not exist "*.nds" ECHO !Error! No nds ROM to repack, press any key to close this window. 2>nul
+if not exist "*.nds" pause >nul 2>nul
+if not exist "*.nds" exit >nul 2>nul
+
+if exist "repacked.nds" pause >nul 2>nul
+if exist "repacked.nds" exit >nul2>nul
+
 ECHO Unpacking...
+@RD /S /Q NDS_UNPACK 2>nul
 mkdir NDS_UNPACK
-rd /Q /S NDS_UNPACK
+rd /Q /S NDS_UNPACK 2>nul
 mkdir NDS_UNPACK
->nul 2>nul ndstool -v -x current.nds -9 NDS_UNPACK/arm9.bin -7 NDS_UNPACK/arm7.bin -y9 NDS_UNPACK/y9.bin -y7 NDS_UNPACK/y7.bin -d NDS_UNPACK/data -y NDS_UNPACK/overlay -t NDS_UNPACK/banner.bin -h NDS_UNPACK/header.bin 
+>nul 2>nul ndstool -v -x *.nds -9 NDS_UNPACK/arm9.bin -7 NDS_UNPACK/arm7.bin -y9 NDS_UNPACK/y9.bin -y7 NDS_UNPACK/y7.bin -d NDS_UNPACK/data -y NDS_UNPACK/overlay -t NDS_UNPACK/banner.bin -h NDS_UNPACK/header.bin 
+
 ECHO Repacking...
->nul ndstool -c repacked.nds -9 NDS_UNPACK/arm9.bin -7 NDS_UNPACK/arm7.bin -y9 NDS_UNPACK/y9.bin -y7 NDS_UNPACK/y7.bin -d NDS_UNPACK/data -y NDS_UNPACK/overlay -t NDS_UNPACK/banner.bin -h NDS_UNPACK/header.bin 
+>nul 2>nul ndstool -c repacked.nds -9 NDS_UNPACK/arm9.bin -7 NDS_UNPACK/arm7.bin -y9 NDS_UNPACK/y9.bin -y7 NDS_UNPACK/y7.bin -d NDS_UNPACK/data -y NDS_UNPACK/overlay -t NDS_UNPACK/banner.bin -h NDS_UNPACK/header.bin 
 @RD /S /Q NDS_UNPACK
-ECHO DONE! (You can close this window now.)
+ECHO DONE! Press any key to close this window.
 pause >nul
